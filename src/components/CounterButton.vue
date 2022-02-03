788f780a15cd33d142eebbe7653e544ac4af62bd
Erika Alexandersson <alexandersson.erika@gmail.com>
2022-02-03 14:39:38 +0100

    Ändrade <p> i CounterButton

--- src/components/CounterButton.vue
+++ src/components/CounterButton.vue
@@ -1,7 +1,11 @@
+<script>
+  import store from '../store'
+</script>
 <template>
   <input
     type="button"
     :value="$store.state.counter"
     @click="$store.commit('increment')"
   />
+  <p>Erikas ändring</p>
 </template>
