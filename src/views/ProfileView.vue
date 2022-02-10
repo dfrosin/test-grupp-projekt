da25e1b002fe0b5b9f77019a59d7ba21d391a674
Erika Alexandersson <alexandersson.erika@gmail.com>
2022-02-10 10:02:03 +0100

    extends prettier

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,12 @@
+<template>
+  <CounterButton />
+</template>
+
+<script>
+  import CounterButton from '../components/CounterButton.vue'
+  export default {
+    components: { CounterButton }
+  }
+</script>
+
+<style lang="scss" scoped></style>
