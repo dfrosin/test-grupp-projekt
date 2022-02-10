626ccade9e2c4bff32b69f3398b3902e97a49d93
Erika Alexandersson <alexandersson.erika@gmail.com>
2022-02-10 22:03:44 +0100

    component added to view

---
+++ src/views/CreateProductBacklogView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <PBIComponent />
+  </div>
+</template>
+
+<script>
+  import PBIComponent from '../components/PBIComponent.vue'
+  export default {
+    components: { PBIComponent }
+  }
+</script>
+
+<style lang="scss" scoped></style>
