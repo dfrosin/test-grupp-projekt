11676145954a31732d1a2e898966bff3603a1792
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-02-10 18:33:14 +0100

    added button and input, sitting on bus and feels like i wanna puke so will stop coding for today

---
+++ src/views/CreateSprintView.vue
@@ -0,0 +1,22 @@
+<script>
+  import CreateInputButton from '../components/CreateInputButton.vue'
+  export default {
+    components: {
+      CreateInputButton
+    }
+  }
+</script>
+
+<template>
+  <h2>CreateSprintView.vue</h2>
+  <CreateInputButton />
+</template>
+
+<style lang="scss" scoped>
+  h2 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5rem;
+  }
+</style>
