ba6ef9656b85b661a910e6af63d5efcfa170baf9
Adam Agerling <alltidvakenhehe@gmail.com>
2022-02-11 09:38:26 +0100

    Add global footer component, for testing purposes

---
+++ src/components/CrumbleFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><p>© 2022 Crumble</p></div>
+</template>
+
+<style lang="scss" scoped>
+  div {
+    display: flex;
+    justify-content: center;
+  }
+  p {
+    font-size: 31px;
+    display: flex;
+  }
+</style>
