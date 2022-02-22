120bfd670fe7c493a051943667e8b4ebafd76da1
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-02-22 16:08:24 +0100

    fixed the code structure

--- src/components/LogoutUser.vue
+++ src/components/LogoutUser.vue
@@ -1,7 +1,3 @@
-<template>
-  <button @click="handleLogout" type="submit" class="btn">Log out</button>
-</template>
-
 <script>
   import { auth } from '../firebase'
 
@@ -19,6 +15,9 @@
     }
   }
 </script>
+<template>
+  <button @click="handleLogout" type="submit" class="btn">Log out</button>
+</template>
 
 <style lang="scss" scoped>
   @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
