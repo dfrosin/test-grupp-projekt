e9f0f826aa1c196e4098df39ee3397f785e6b930
David Forsyth Rosin <drosin96@gmail.com>
2022-03-02 10:58:45 +0100

    add pbi showcase

--- src/views/SprintTestTestTestView.vue
+++ src/views/SprintTestTestTestView.vue
@@ -49,6 +49,8 @@
         searchQuery: '',
         editId: '',
         columnCounter: 0,
+        pbiArray: [],
+        pbiHover: null,
         test: []
       }
     },
@@ -102,6 +104,19 @@
           this.arrayOfProjectNames = projectNames
         })
       },
+      getPbi() {
+        const allPbi = query(
+          collection(firestore, `${this.projectName}`),
+          where('PBI', '==', 'PBI'),
+          limit(50)
+        )
+
+        getDocs(allPbi).then((snapshot) => {
+          snapshot.docs.forEach((doc) => {
+            this.pbiArray.push({ ...doc.data(), id: doc.id })
+          })
+        })
+      },
       detectMove(evt) {
         this.printTimestamp()
         //hämtar namnet på columnen via CSS id
@@ -126,6 +141,7 @@
         const projectDoc = doc(firestore, `projects/${this.projectName}`)
         const projectSnapshot = await getDoc(projectDoc)
         this.projectInfo = projectSnapshot.data()
+        this.getPbi()
 
         this.getDatabase()
       },
@@ -221,301 +237,369 @@
 </script>
 
 <template>
-  <KanbanUsers />
-  <AddNewTask @send-task="getTask" />
-  <div v-if="this.arrayOfProjectNames !== null">
-    <select @change="selectProjectName">
-      <option>Select a project</option>
-      <option v-for="projects in this.arrayOfProjectNames" :key="projects.id">
-        {{ projects.id }}
-      </option>
-    </select>
-    <h2 v-if="select">Project: {{ this.projectName }}</h2>
-  </div>
-  <AddNewTask
-    v-if="projectInfo !== null"
-    @send-task="getTask"
-    :first-status="projectInfo.status[0]"
-  />
-  <div v-if="projectInfo !== null" class="search-container">
-    <input
-      @input="filterSearch"
-      type="text"
-      placeholder="Type here"
-      v-model="searchQuery"
+  <div class="kanban-board">
+    <KanbanUsers />
+    <AddNewTask @send-task="getTask" />
+    <div v-if="this.arrayOfProjectNames !== null">
+      <select @change="selectProjectName">
+        <option>Select a project</option>
+        <option v-for="projects in this.arrayOfProjectNames" :key="projects.id">
+          {{ projects.id }}
+        </option>
+      </select>
+      <h2 v-if="select">Project: {{ this.projectName }}</h2>
+    </div>
+    <AddNewTask
+      v-if="projectInfo !== null"
+      @send-task="getTask"
+      :first-status="projectInfo.status[0]"
     />
-    {{ sortTest }}
-  </div>
-
-  <article class="flex-container">
-    <p
-      v-if="projectName != null"
-      class="add-column"
-      @click="addColumn"
-      @mouseover="addHover = true"
-      @mouseleave="addHover = false"
-    >
-      +
-    </p>
-    <p v-if="addHover" class="tool-tip">Add a new column</p>
-    <sprint-list
-      class="column"
-      v-for="object in anotherArray"
-      :key="object.cards"
-      :title="object.title"
-    >
-      <p
-        v-if="object.cards.length < 1 && !editStatus"
-        @click="editColumn"
-        class="edit-column"
-        :id="object.title"
-      >
-        edit
-      </p>
-      <p
-        v-if="
-          object.cards.length < 1 &&
-          editStatus &&
-          statusInput.length > 0 &&
-          statusInput.length < 14 &&
-          editId === object.title
-        "
-        @click="saveColumn"
-        class="save-column"
-        :id="object.title"
-      >
-        save
-      </p>
-      <p
-        class="save-column"
-        id="save-disabled"
-        v-if="
-          object.cards.length < 1 &&
-          (statusInput.length < 1 || statusInput.length > 14) &&
-          editStatus &&
-          editId === object.title
-        "
-      >
-        save
-      </p>
-      <p
-        v-if="
-          object.cards.length < 1 &&
-          editStatus &&
-          statusInput.length < 1 &&
-          editId === object.title
-        "
-        class="input-warning"
-      >
-        You can't have an empty title
-      </p>
-      <p
-        v-if="object.cards.length < 1 && editStatus && statusInput.length > 14"
-        class="input-warning"
-      >
-        Title is too long
-      </p>
-      <p
-        v-if="object.cards.length < 1 && editStatus && editId === object.title"
-        placeholder="Column Title"
-        @click="deleteColumn"
-        class="delete-column"
-        :id="object.title"
-      >
-        delete
-      </p>
+    <div v-if="projectInfo !== null" class="search-container">
       <input
-        v-if="object.cards.length < 1 && editStatus && editId === object.title"
-        v-model="statusInput"
+        @input="filterSearch"
         type="text"
-        class="status-input"
-        autofocus
+        placeholder="Type here"
+        v-model="searchQuery"
       />
-      <section :id="object.title" class="sections">
-        <draggable
-          :list="object.cards"
-          :move="(event) => detectMove(event, 123)"
-          group="walla"
-          ghost-class="on-drag"
-          class="drop-zone-height"
+      {{ sortTest }}
+    </div>
+    <div v-if="projectInfo !== null" class="pbi-container">
+      <p>PBI:</p>
+      <div
+        v-for="pbi in pbiArray"
+        class="pbi-color"
+        :key="pbi.id"
+        :style="{ backgroundColor: pbi.color }"
+        @mouseover="pbiHover = pbi.docId"
+        @mouseleave="pbiHover = null"
+      >
+        <p
+          :id="pbi.docId"
+          v-if="pbiHover === pbi.docId"
+          :style="{ borderColor: pbi.color }"
         >
-          <sprint-card
-            v-for="card in object.cards"
-            :key="card.id"
-            :item="card"
-            class="drag-element"
-          />
-        </draggable>
-      </section>
-    </sprint-list>
-  </article>
+          {{ pbi.id }}
+        </p>
+      </div>
+    </div>
+    <article class="flex-container">
+      <p
+        v-if="projectName != null"
+        class="add-column"
+        @click="addColumn"
+        @mouseover="addHover = true"
+        @mouseleave="addHover = false"
+      >
+        +
+      </p>
+      <p v-if="addHover" class="tool-tip">Add a new column</p>
+      <sprint-list
+        class="column"
+        v-for="object in anotherArray"
+        :key="object.cards"
+        :title="object.title"
+      >
+        <p
+          v-if="object.cards.length < 1 && !editStatus"
+          @click="editColumn"
+          class="edit-column"
+          :id="object.title"
+        >
+          edit
+        </p>
+        <p
+          v-if="
+            object.cards.length < 1 &&
+            editStatus &&
+            statusInput.length > 0 &&
+            statusInput.length < 14 &&
+            editId === object.title
+          "
+          @click="saveColumn"
+          class="save-column"
+          :id="object.title"
+        >
+          save
+        </p>
+        <p
+          class="save-column"
+          id="save-disabled"
+          v-if="
+            object.cards.length < 1 &&
+            (statusInput.length < 1 || statusInput.length > 14) &&
+            editStatus &&
+            editId === object.title
+          "
+        >
+          save
+        </p>
+        <p
+          v-if="
+            object.cards.length < 1 &&
+            editStatus &&
+            statusInput.length < 1 &&
+            editId === object.title
+          "
+          class="input-warning"
+        >
+          You can't have an empty title
+        </p>
+        <p
+          v-if="
+            object.cards.length < 1 && editStatus && statusInput.length > 14
+          "
+          class="input-warning"
+        >
+          Title is too long
+        </p>
+        <p
+          v-if="
+            object.cards.length < 1 && editStatus && editId === object.title
+          "
+          placeholder="Column Title"
+          @click="deleteColumn"
+          class="delete-column"
+          :id="object.title"
+        >
+          delete
+        </p>
+        <input
+          v-if="
+            object.cards.length < 1 && editStatus && editId === object.title
+          "
+          v-model="statusInput"
+          type="text"
+          class="status-input"
+          autofocus
+        />
+        <section :id="object.title" class="sections">
+          <draggable
+            :list="object.cards"
+            :move="(event) => detectMove(event, 123)"
+            group="walla"
+            ghost-class="on-drag"
+            class="drop-zone-height"
+          >
+            <sprint-card
+              v-for="card in object.cards"
+              :key="card.id"
+              :item="card"
+              class="drag-element"
+            />
+          </draggable>
+        </section>
+      </sprint-list>
+    </article>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-  .search-container {
-    display: flex;
-    flex-direction: column;
-    width: 25%;
-    margin: 2%;
-  }
-  select {
-    font-size: 1.2rem;
-    padding: 1rem;
-    border-radius: 10px;
-    margin-left: 4rem;
-  }
-  h2 {
-    margin-left: 4rem;
-    color: white;
-  }
-  h3 {
-    color: white;
-    margin: 3rem 0 1rem 0;
-  }
-  .flex-container {
-    background-color: rgba(235, 235, 235, 0.3);
-    margin: 30px auto;
-    max-width: 80%;
-    padding-top: 20px;
-    display: flex;
-    grid-template-columns: auto;
-    justify-content: space-evenly;
-    text-align: center;
-    border-radius: 10px;
-    flex-wrap: wrap;
-    margin-bottom: 1rem;
+  .kanban-board {
     position: relative;
-    .add-column {
-      color: white;
-      font-size: 2rem;
-      position: absolute;
-      top: 2px;
-      right: 10px;
-      cursor: pointer;
+    .search-container {
+      display: flex;
+      flex-direction: column;
+      width: 25%;
+      margin: 2%;
     }
-    .tool-tip {
-      position: absolute;
-      width: 50px;
-      background-color: rgba(235, 235, 235, 0.3);
-      color: white;
-      padding: 5px;
+    select {
+      font-size: 1.2rem;
+      padding: 1rem;
       border-radius: 10px;
-      top: -60px;
-      right: 0;
+      margin-left: 4rem;
     }
-    .add-column:hover {
-      color: aqua;
-    }
-    .edit-column {
-      cursor: pointer;
-      position: absolute;
+    h2 {
+      margin-left: 4rem;
       color: white;
-      text-transform: uppercase;
-      top: -14px;
-      right: 45%;
     }
-    .save-column {
-      cursor: pointer;
-      position: absolute;
+    h3 {
       color: white;
-      text-transform: uppercase;
-      top: -14px;
-      right: 55%;
+      margin: 3rem 0 1rem 0;
     }
-    #save-disabled {
-      color: rgb(104, 104, 104);
-      cursor: not-allowed;
-    }
-    #save-disabled:hover {
-      text-decoration: none;
-    }
-    .delete-column {
-      cursor: pointer;
+    .pbi-container {
+      z-index: 500;
+      top: 20%;
+      left: 3%;
       position: absolute;
-      color: rgb(255, 255, 255);
-      text-transform: uppercase;
-      top: -14px;
-      right: 28%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border-radius: 20px;
+      gap: 10px;
+      padding-bottom: 5px;
+      padding-top: 5px;
+      background-color: rgba(235, 235, 235, 0.3);
+      width: 30px;
+      color: white;
+      p {
+        font-size: 1.2rem;
+      }
+      .pbi-color {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        position: relative;
+        border: 2px solid rgba(0, 0, 0, 0.3);
+        p {
+          box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2),
+            0 8px 22px 0 rgba(0, 0, 0, 0.19);
+          border: 2px solid;
+          color: black;
+          background-color: #fff;
+          border-radius: 10px;
+          width: 200px;
+          position: absolute;
+          left: 30px;
+          padding: 5px;
+        }
+      }
     }
-    .status-input {
-      position: absolute;
-      font-size: 1.5rem;
-      background-color: rgb(229, 190, 255);
-      top: 0px;
-      left: 5%;
-      width: 90%;
+    .flex-container {
+      background-color: rgba(235, 235, 235, 0.3);
+      margin: 30px auto;
+      max-width: 80%;
+      padding-top: 20px;
+      display: flex;
+      grid-template-columns: auto;
+      justify-content: space-evenly;
       text-align: center;
       border-radius: 10px;
+      flex-wrap: wrap;
+      margin-bottom: 1rem;
+      position: relative;
+      .add-column {
+        color: white;
+        font-size: 2rem;
+        position: absolute;
+        top: 2px;
+        right: 10px;
+        cursor: pointer;
+      }
+      .tool-tip {
+        position: absolute;
+        width: 50px;
+        background-color: rgba(235, 235, 235, 0.3);
+        color: white;
+        padding: 5px;
+        border-radius: 10px;
+        top: -60px;
+        right: 0;
+      }
+      .add-column:hover {
+        color: aqua;
+      }
+      .edit-column {
+        cursor: pointer;
+        position: absolute;
+        color: white;
+        text-transform: uppercase;
+        top: -14px;
+        right: 45%;
+      }
+      .save-column {
+        cursor: pointer;
+        position: absolute;
+        color: white;
+        text-transform: uppercase;
+        top: -14px;
+        right: 55%;
+      }
+      #save-disabled {
+        color: rgb(104, 104, 104);
+        cursor: not-allowed;
+      }
+      #save-disabled:hover {
+        text-decoration: none;
+      }
+      .delete-column {
+        cursor: pointer;
+        position: absolute;
+        color: rgb(255, 255, 255);
+        text-transform: uppercase;
+        top: -14px;
+        right: 28%;
+      }
+      .status-input {
+        position: absolute;
+        font-size: 1.5rem;
+        background-color: rgb(229, 190, 255);
+        top: 0px;
+        left: 5%;
+        width: 90%;
+        text-align: center;
+        border-radius: 10px;
+      }
+      .input-warning {
+        position: absolute;
+        background-color: rgba(255, 0, 0, 0.3);
+        color: rgb(251, 251, 251);
+        padding: 5px;
+        border-radius: 10px;
+        top: -50px;
+        width: 100%;
+      }
+      .edit-column:hover,
+      .save-column:hover {
+        color: aqua;
+        text-decoration: underline;
+      }
+      .delete-column:hover {
+        color: rgb(255, 164, 164);
+        text-decoration: underline;
+      }
     }
-    .input-warning {
-      position: absolute;
-      background-color: rgba(255, 0, 0, 0.3);
-      color: rgb(251, 251, 251);
-      padding: 5px;
-      border-radius: 10px;
-      top: -50px;
-      width: 100%;
-    }
-    .edit-column:hover,
-    .save-column:hover {
-      color: aqua;
-      text-decoration: underline;
-    }
-    .delete-column:hover {
-      color: rgb(255, 164, 164);
-      text-decoration: underline;
-    }
-  }
-  .column {
-    min-width: 180px;
-    width: 20%;
-    max-width: 300px;
-    position: relative;
-    margin: 0 1%;
-  }
-  .sections {
-    background-color: rgba(249, 249, 249, 0.564);
-    border-radius: 10px;
-    flex-wrap: wrap;
-    margin-bottom: 5rem;
-    padding: 6px;
-    position: relative;
-  }
-
-  .drag-element {
-    font-size: 1.5rem;
-    text-align: center;
-    color: black;
-    width: 90%;
-    background-color: white;
-    border-radius: 10px;
-    border: 3px solid;
-    min-height: 10px;
-    cursor: grab;
-    position: relative;
-    margin: 5% 5%;
-  }
-  .drag-element:hover {
-    box-shadow: 0 0 5px;
-  }
-  .on-drag {
-    background-color: rgb(81, 89, 194);
-    color: #fff;
-    z-index: 10;
-  }
-  .drop-zone-height {
-    height: 60vh;
-    padding-bottom: 10%;
-    overflow: scroll;
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-  }
-  .drop-zone-height::-webkit-scrollbar {
-    display: none;
-  }
-  @media screen and (max-width: 300px) {
     .column {
-      min-width: 80%;
+      min-width: 180px;
+      width: 20%;
+      max-width: 300px;
+      position: relative;
+      margin: 0 1%;
+    }
+    .sections {
+      background-color: rgba(249, 249, 249, 0.564);
+      border-radius: 10px;
+      flex-wrap: wrap;
+      margin-bottom: 5rem;
+      padding: 6px;
+      position: relative;
+    }
+
+    .drag-element {
+      font-size: 1.5rem;
+      text-align: center;
+      color: black;
+      width: 90%;
+      background-color: white;
+      border-radius: 10px;
+      border: 3px solid;
+      min-height: 10px;
+      cursor: grab;
+      position: relative;
+      margin: 5% 5%;
+    }
+    .drag-element:hover {
+      box-shadow: 0 0 5px;
+    }
+    .on-drag {
+      background-color: rgb(81, 89, 194);
+      color: #fff;
+      z-index: 10;
+    }
+    .drop-zone-height {
+      height: 60vh;
+      padding-bottom: 10%;
+      overflow: scroll;
+      -ms-overflow-style: none;
+      scrollbar-width: none;
+    }
+    .drop-zone-height::-webkit-scrollbar {
+      display: none;
+    }
+    @media screen and (max-width: 300px) {
+      .column {
+        min-width: 80%;
+      }
     }
   }
 </style>
